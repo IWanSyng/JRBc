4fc8e45f03b668f182997161939659d7e4a61586
98-f355-f1 <tomspharmacy@yahoo.com>
2021-03-16 19:53:12 +0200

    First commit from Tom

--- src/main/java/work/iwansyng/iwansyng/IwansyngApplication.java
+++ src/main/java/work/iwansyng/iwansyng/IwansyngApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IwansyngApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(IwansyngApplication.class, args);
 		System.out.println("Hello from Maris!");
