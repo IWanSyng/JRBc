01433fb7dae94bb88737313bf9e7a39a35021878
Lauris Melderis <lauris.melderis77@gmail.com>
2021-03-16 18:24:07 +0200

    Initial commit

---
+++ src/main/java/work/iwansyng/iwansyng/IwansyngApplication.java
@@ -0,0 +1,13 @@
+package work.iwansyng.iwansyng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwansyngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwansyngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/iwansyng/iwansyng/IwansyngApplicationTests.java
@@ -0,0 +1,13 @@
+package work.iwansyng.iwansyng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwansyngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
