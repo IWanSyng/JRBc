9f80b64d3152d0cb96d4114df8e5912ffe2ebba0
98-f355-f1 <saleenseeker@gmail.com>
2021-03-23 21:38:03 +0200

    Moved /user/home GET to UserController

--- src/main/java/work/iwansyng/iwansyng/controller/UserController.java
+++ src/main/java/work/iwansyng/iwansyng/controller/UserController.java
@@ -4,33 +4,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import work.iwansyng.iwansyng.model.role.User;
-import work.iwansyng.iwansyng.repository.ConfigParamRepository;
 import work.iwansyng.iwansyng.service.IwanSyngUserService;
 
-import javax.validation.Valid;
-
 @Controller
+@RequestMapping(path = "/user")
 @RequiredArgsConstructor
-public class LoginController {
+public class UserController {
 
     private final IwanSyngUserService userService;
 
-    private final ConfigParamRepository configParamRepository;
-
-    @GetMapping(value = "/")
-    public String index() {
-        return "index";
-    }
-
-    @GetMapping(value= "/login")
-    public ModelAndView login(){
+    @GetMapping(value="/home")
+    public ModelAndView homeUser(){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByUserName(auth.getName());
+        modelAndView.addObject("userName", "Welcome " + user.getUsername() + "/" + user.getFirstName() + " " + user.getLastName());
+        modelAndView.addObject("userMessage","Content Available Only for Users with User Role");
+        modelAndView.setViewName("user/home");
+
         return modelAndView;
     }
 }
