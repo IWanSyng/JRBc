9dfd4dcc7e5d834b702cbfce529df965f8ebc132
98-f355-f1 <saleenseeker@gmail.com>
2021-03-24 17:19:03 +0200

    User dashboard 60% implemented

--- src/main/java/work/iwansyng/iwansyng/controller/UserController.java
+++ src/main/java/work/iwansyng/iwansyng/controller/UserController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import work.iwansyng.iwansyng.model.Course;
+import work.iwansyng.iwansyng.model.quiz.Quiz;
 import work.iwansyng.iwansyng.model.role.Role;
+import work.iwansyng.iwansyng.model.role.Student;
 import work.iwansyng.iwansyng.model.role.User;
 import work.iwansyng.iwansyng.repository.CourseRepository;
 import work.iwansyng.iwansyng.repository.InstructorRepository;
@@ -21,6 +23,7 @@ import work.iwansyng.iwansyng.service.IwanSyngUserService;
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(path = "/user")
@@ -61,7 +64,7 @@ public class UserController {
 
     //@RequestMapping(value = "/dashboard", params = { "id", "Username" }, method = RequestMethod.GET)
     @RequestMapping(value = "/dashboard/{Username}", method = RequestMethod.GET)
-    public ModelAndView adminHomePage(@PathVariable("Username") String username, Principal principal) {
+    public ModelAndView userHomePage(@PathVariable("Username") String username, Principal principal) {
         Optional<String> name = getOptionalName(username);
         ModelAndView modelAndView = new ModelAndView();
 
@@ -72,15 +75,41 @@ public class UserController {
 
         User user = userRepository.findByUsername(name.get());
 
-//        modelAndView.addObject("userName", "Welcome " + user.getUsername() + "/" + user.getFirstName() + " " + user.getLastName());
-//        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        Student s = new Student();
+        s.setCourse(courseRepository.findCourseById(13L).get());
+        s.setUniqueId((Integer) 19009);
+        s.setUser(user);
+        studentRepository.save(s);
 
-        List<Course> courses = courseRepository.findAll();
+        s = new Student();
+        s.setCourse(courseRepository.findCourseById(6L).get());
+        s.setUniqueId((Integer) 19010);
+        s.setUser(user);
+
+        studentRepository.save(s);
+
+        List<Student> students = studentRepository.findAll()
+                .stream()
+                .filter((st) -> st.getUser().getId().equals(user.getId()))
+                .collect(Collectors.toList());
+
+        List<Course> allCourses = courseRepository.findAll();;
+        modelAndView.addObject("courses", allCourses);
+
         Optional<Role> role = Optional.ofNullable(user.getRoles().stream().findFirst().orElse(null));
+
+        Optional<Student> student = studentRepository.findById(s.getId());
+        if (student.isPresent()) {
+            // get a list of courses where the student.user.id == user.id
+            List<Course> coursesEnrolled = courseRepository.findAll()
+                    .stream()
+                    .filter(c -> c.getId().equals(student.get().getCourse().getId()))
+                    .collect(Collectors.toList());
+            modelAndView.addObject("coursesEnrolled", coursesEnrolled);
+        }
 
         modelAndView.addObject("user", user);
         modelAndView.addObject("role", (role.isPresent() ? role.get().getRole() : ""));
-        modelAndView.addObject("courses", courses);
         modelAndView.setViewName("user/user_dashboard");
 
         return modelAndView;
