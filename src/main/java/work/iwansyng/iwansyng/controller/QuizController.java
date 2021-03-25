b5ef638089ca1879c03c3b12230bf20728764ca3
Maris Melbardis <689628+FirstPlatoLV@users.noreply.github.com>
2021-03-25 23:04:28 +0200

    Quiz views work

--- src/main/java/work/iwansyng/iwansyng/controller/QuizController.java
+++ src/main/java/work/iwansyng/iwansyng/controller/QuizController.java
@@ -27,7 +27,7 @@ public class QuizController {
     private final UserRepository userRepository;
     private final CourseRepository courseRepository;
 
-    @GetMapping(path = "/quiz_preview")
+    @GetMapping(path = "/quiz_view")
     public String addQuiz(Model model) {
         Quiz quiz = new Quiz();
         quiz.setName("First quiz");
@@ -40,10 +40,6 @@ public class QuizController {
         QuizItem firstQuestion = new QuizItem(AnswerType.SINGLE, "What is your name?", "Lauris", "Santa", "Maris", "Tom");
 
         QuizItem secondQuestion = new QuizItem(AnswerType.MULTIPLE, "What is your name?", "Lauris2", "Santa2", "Maris2", "Tom2");
-//        secondQuestion.questionAnswerMap.put("Lauris2", "");
-//        secondQuestion.questionAnswerMap.put("Santa2", "");
-//        secondQuestion.questionAnswerMap.put("Maris2", "");
-//        secondQuestion.questionAnswerMap.put("Tom2", "");
 
         quiz.addQuizItem(firstQuestion);
         quiz.addQuizItem(secondQuestion);
@@ -51,7 +47,7 @@ public class QuizController {
 
         quizRepository.save(quiz);
 
-        return "quiz_preview";
+        return "quiz_view";
     }
 
     // retrieve from the database for admin view
@@ -74,7 +70,7 @@ public class QuizController {
         return modelAndView;
     }
 
-    @PostMapping("/quiz_preview")
+    @PostMapping("/quiz_view")
     public String submitTest(@ModelAttribute(value = "quiz") Quiz quiz, Model model) {
 //        System.out.println(quiz);
         for (QuizItem quizItem : quiz.getQuizItems()) {
@@ -85,23 +81,6 @@ public class QuizController {
 
         quizRepository.save(quiz);
 
-        return "quiz_preview";
+        return "quiz_view";
     }
-
-//    @GetMapping(path = "/quiz_test", produces= MediaType.APPLICATION_JSON_VALUE)
-//    public ModelAndView testQuizController() {
-//        List<Quiz> quizzes = quizRepository.findAll()
-//                .stream()
-//                .filter(q -> q.getCourse().getId().equals(6L))
-//                .collect(Collectors.toList());
-//
-//
-//        Quiz quiz = quizzes.get(0);
-//        Quiz quizClone = quiz.getQuizWithoutAnswers();
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.addObject(quiz);
-//        modelAndView.setViewName("quiz_test");
-//
-//        return modelAndView;
-//    }
 }
